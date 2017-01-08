42afcc2b9cb5a6eb99bee7ecc4556a81e46f6251
disinuo <776550026@qq.com>
2017-01-08 22:36:03 +0800

    parseFloat()~

--- Number.js
+++ Number.js
@@ -6,7 +6,7 @@ var x0='22s';   //22---------1
 var x1='a22';   //NaN--------2
 var x2='22.413';//22---------3
 var x3='';      //NaN--------4
-var x4='-22s';  //-22--------5
+var x4='-22e5';  //-22--------5
 var intX=parseInt(x4);
 console.log(intX);
 //-----------------------
@@ -31,3 +31,21 @@ console.log(intZ);
 //Also notice that string like `0x10` when be transferred with a second parameter 8,
 //the transfer will stop at `x`---10,11
 //*******************************
+console.log("*********************");
+//************ parseFloat() ******************************
+var a0='092.5';     //92.5---------1
+var a1='23.4.5';    //23.4---------2
+var a2='23.4g';     //23.4---------3
+var a3='s23.4.5';   //NaN----------4
+var a4='3.6e5';     //360000-------5
+var a5='3.6e-5';    //0.000036-----6
+var a6='-3.6';      //-3.6---------7
+var floatA=parseFloat(a6);
+console.log(floatA);
+//==============conclusion==================
+//1.it will translate until the character that is not a number or `.`or `e` or `E`---3,5,6
+//2.only the first `.` will be identified.
+// (the second`.` and characters following it will be ignored)---2
+//3.It can only identify string starting with + or - or number,otherwise return `NaN`---4,7
+//4.starting 0(s) will be ignored---1
+//********************************************************
